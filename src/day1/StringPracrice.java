52e8bfbc19e9e4d1b3692d77081db99578411d30
Israfil13 <hajiyevisrafil@gmail.com>
2020-02-29 13:00:17 -0600

    "My first commit"

--- src/day1/StringPracrice.java
+++ src/day1/StringPracrice.java
@@ -2,6 +2,8 @@ package day1;
 
 public class StringPracrice {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello " +
+                "world");
+        System.out.println("bye bye");
     }
 }
