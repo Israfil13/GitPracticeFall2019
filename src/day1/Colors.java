53e875680791de0e6da0404e21dd006cb6984d34
Israfil13 <hajiyevisrafil@gmail.com>
2020-03-11 19:31:52 -0500

    Changed

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -3,5 +3,6 @@ package day1;
 public class Colors {
     public static void main(String[] args) {
         String []colors={"red","blue","orange","yellow"};
+        System.out.println("My color");
     }
 }
